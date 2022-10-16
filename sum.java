826e8fdda9b869263f20d7f37c73a09df850dca9
Karthik-Katuri <kkaturi2003@gmail.com>
2022-10-16 22:02:40 +0530

    program 2 : sum of two numbers sum.java

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Sum
+{
+public static void main (String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("enter first number");
+int a = sc.nextInt();
+System.out.println("enter second number");
+int b = sc.nextInt();
+int sum = a+b;
+System.out.println("sum of two numbers is "+ sum);
+}
+
+}
