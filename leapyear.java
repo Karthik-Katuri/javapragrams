ee6c23dcafe40d477f3387d72352b0bb9c206adc
Karthik-Katuri <kkaturi2003@gmail.com>
2022-10-16 21:29:37 +0530

    program 1 : leap yearleapyear.java

---
+++ leapyear.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Leapyear
+{
+public static void main (String args[])
+{
+Scanner sc= new Scanner(System.in);
+system.out.println("enter a year");
+int a = sc.nextInt();
+if(a%4==0)
+{
+system.out.println("leap year");
+}
+else
+{
+system.out.println("not a leap year");
+}
+}
+}
+
